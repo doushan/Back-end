0f4d20166802aa56a52255521d5d081774337cf8
Dusyant Goolab <dusyant3993@gmail.com>
2016-07-28 11:30:33 +0400

    StaffController and VacancyController Added

---
+++ C#/ProductsApp/Controllers/StaffController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+    public class StaffController : ApiController
+    {
+        Staff[] _staff  = new Staff[]
+        {
+            new Staff{Id = 1, FirstName = "Doushan", LastName = "Goolab", Username = "ddd", Password = "1234", ConfirmPassword = "1234", PhoneNumber = "14253628", ResidenceNumber = "7896321", Email = "doushan@live.com", Address = "Vacoas", JobTitle = "Developer"},
+            new Staff{Id = 1, FirstName = "Bonie", LastName = "Coder", Username = "bbb", Password = "555", ConfirmPassword = "555", PhoneNumber = "78963254", ResidenceNumber = "7896325", Email = "bonie@live.com", Address = "Quatre Borne", JobTitle = "Developer"}
+           
+        };
+
+        public IEnumerable<Staff> GetAllStaff()
+        {
+            return _staff;
+        }
+
+        public IHttpActionResult GetStaff(int id)
+        {
+            var staff = _staff.FirstOrDefault((p) => p.Id == id);
+            if (_staff == null)
+            {
+                return NotFound();
+            }
+            return Ok(_staff);
+        }
+    }
+}
