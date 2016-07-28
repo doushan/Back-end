b5dd50777e6958a50a147e2569e53d7dc65397b9
Umar Peerboccus <umarpeerboccus@gmail.com>
2016-07-28 11:16:36 +0400

    Interview Class and Interview Controller added
    Umar

---
+++ C#/ProductsApp/Models/Interview.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductsApp.Models
+{
+    public class Interview
+    {
+        public String JobTitle { get; set; }
+        public String Department { get; set; }
+        public String DateApplied { get; set; }
+    }
+}
