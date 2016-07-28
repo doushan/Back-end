2164e31162da021351f67125df2700dd4d390901
Dusyant Goolab <dusyant3993@gmail.com>
2016-07-28 11:04:34 +0400

    Staff and Vacancy Class Added

---
+++ C#/ProductsApp/Models/Vacancy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductsApp.Models
+{
+    public class Vacancy
+    {
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public string Position { get; set; }
+        public string Description { get; set; }
+        public string Skills { get; set; }
+        public string Department { get; set; }
+    }
+}
